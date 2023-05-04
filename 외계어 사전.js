cb453799d5db07c065fc033dc89f868f5ccc7fbd
Youn yeochan <ducks1077@gmail.com>
2023-05-04 18:02:00 +0900

    2023.05.04
    2023.05.04

--- 외계어 사전.js
+++ 외계어 사전.js
@@ -27,15 +27,17 @@ spell	                dic	                                    result
 let spell = ["s", "o", "m", "d"];
 let dic = ["moos", "dzx", "smm", "sunmmo", "som"];	
 
-function solution(spell, dic) {
-    // dic.filter를 이용하여 배열의 every의 값을 담는다. 하지만 빈값이 담길 경우 1이 반환되는 문제 발생
-    // 그래서 length 빈값인지 확인한 후 비었을때 2 하나라도 있을 경우 1 every를 사용하면 전부 트루 일때는 트루를 반환한다 includes는 트루와 펄스를 반환하니깐 전부 트루가 나오면 해당 1 아니면 2
-    // return dic.filter(a => spell.every(el=> a.includes(el))).length ? 1 : 2;
-    return dic.filter(a => spell.every(el=> a.includes(el))).length ? 1 : 2;
-}
-
 // function solution(spell, dic) {
-//     return dic.some(a => spell.sort() + '' == [...a].sort() + '') ? 1 : 2;
+//     // dic.filter를 이용하여 배열의 every의 값을 담는다. 하지만 빈값이 담길 경우 1이 반환되는 문제 발생
+//     // 그래서 length 빈값인지 확인한 후 비었을때 2 하나라도 있을 경우 1 every를 사용하면 전부 트루 일때는 트루를 반환한다 includes는 트루와 펄스를 반환하니깐 전부 트루가 나오면 해당 1 아니면 2
+//     // return dic.filter(a => spell.every(el=> a.includes(el))).length ? 1 : 2;
+//     // every의 경우 전부 트루일 경우에만 트루를 반환하기 때문에 내가 확인한 방법은 dic를 필터로 조건을 걸어서 a라는 요소를 순환하며 조건으로 spell의 every를 사용하여 받아온 a라는 값을 el과 비교하여 트루일 경우
+//     //  트루를 반환하게한다. 그렇게해서 하나라도 값이 있을 경우엔 1 아니면 2
+//     return dic.filter(a => spell.every(el=> a.includes(el))).length ? 1 : 2;
 // }
+
+const solution = (spell, dic) => {
+    return dic.some(a => spell.sort() + '' == [...a].sort() + '') ? 1 : 2;
+}
 
 console.log(solution(spell, dic));
